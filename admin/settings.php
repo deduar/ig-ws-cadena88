99819c21128fdd8bc5ed73afeb3e63d5c92fe0b5
Grupo IGAL <eduardo@grupoigal.com>
2021-09-17 15:59:11 +0200

    setting

--- admin/settings.php
+++ admin/settings.php
@@ -11,26 +11,26 @@
         <div clss="wpbody-content">
             <div class="wrap">
                 <div class="col-12">
-                    <h3>Settings here !!!</h3> <?php echo "settings = "; var_dump($settings); ?> 
+                    <h3>Settings here !!!</h3> 
                     <br>
                     <table>
                         <form name="setting" method="post" action="">
                             <tr>
                                 <div class="form-group">
                                     <td><label for="initialWDSL">Initial WSDL</label></td>
-                                    <td><input type="text" class="form-control" name="initialWDSL" id="initialWDSL" value="<?php if(isset($_POST['initialWDSL'])){echo $_POST['initialWDSL'];}else{echo $settings['initialWDSL'];} ?>" size="80"></td>
+                                    <td><input type="text" class="form-control" name="initialWDSL" id="initialWDSL" value="<?php if($_POST['initialWDSL'] != ""){echo $_POST['initialWDSL'];}else{echo $settings['initialWDSL'];} ?>" size="80"></td>
                                 </div>
                             </tr>
                             <tr>
                                 <div class="form-group">
                                     <td><label for="wdslUsername">Username </label></td>
-                                    <td><input type="text" class="form-control" name="wdslUsername" id="wdslUsername" value="<?php if(isset($_POST['wdslUsername'])){echo $_POST['wdslUsername'];}else{echo $settings['wdslUsername'];} ?>"></td>
+                                    <td><input type="text" class="form-control" name="wdslUsername" id="wdslUsername" value="<?php if($_POST['wdslUsername'] != ""){echo $_POST['wdslUsername'];}else{echo $settings['wdslUsername'];} ?>"></td>
                                 </div>
                             </tr>
                             <tr>
                                 <div class="form-group">
                                     <td><label for="wdslPassword">Password </label></td>
-                                    <td><input type="password" class="form-control" name="wdslPassword" id="wdslPassword" value="<?php if(isset($_POST['wdslPassword'])){echo $_POST['wdslPassword'];}else{echo $settings['wdslPassword'];} ?>"></td>
+                                    <td><input type="password" class="form-control" name="wdslPassword" id="wdslPassword" value="<?php if($_POST['wdslPassword'] != ""){echo $_POST['wdslPassword'];}else{echo $settings['wdslPassword'];} ?>"></td>
                                 </div>
                             </tr>
                             <tr>
@@ -53,7 +53,15 @@
 // Proces the form
 
 if (isset($_POST["submit"])) {
-    echo "Process form SUBMIT";
+    echo "Process form SUBMIT <br>";
+    if(($_POST["initialWDSL"] != "") && ($_POST["wdslUsername"] != "") && ($_POST["wdslPassword"] != "")){
+        echo "update reg";
+        echo $_POST["initialWDSL"]."<br>";
+        echo $_POST["wdslUsername"]."<br>";
+        echo $_POST["wdslPassword"]."<br>";
+    }else{
+        echo "Form incompleto";
+    }
 }
 
 ?>
