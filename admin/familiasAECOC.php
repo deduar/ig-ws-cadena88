ecdb780d76335e6bd90baaec73472f84ec8da3e2
Grupo IGAL <eduardo@grupoigal.com>
2021-09-28 12:51:19 +0200

    familias

--- admin/familiasAECOC.php
+++ admin/familiasAECOC.php
@@ -9,46 +9,100 @@
 
 <?php
 
-$file = new SplFileObject(dirname(__FILE__)."/familiaAECOC.xml");
-$category = [];
-$sub_category = [];
-$sub_sub_category = [];
-$sub_sub_sub_category = [];
+$file = new SplFileObject(dirname(__FILE__) . "/familiaAECOC.xml");
+$categories = 0;
+$sub_categories = 0;
+$sub_sub_category = 0;
+$sub_sub_sub_category = 0;
 $i = 1;
 while (!$file->eof()) {
     $data = $file->fgets();
-    switch ($i){
+    switch ($i) {
         case 5:
-            $cat = strtolower(preg_replace('/\s+/', '-',stripAccents(trim($data))));
-            if(!in_array($cat,$category)){$category[] = $cat;}
+            if (strlen($data) != 29) {
+                $catMain = $data;
+                if (!get_term_by('slug', $data, 'product_cat')) {
+                    $cat = strtolower(preg_replace('/\s+/', '-', stripAccents(trim($data))));
+                    wp_insert_term($data, 'product_cat', array(
+                        'description' => $data,
+                        'parent' => 0,
+                        'slug' => $cat
+                    ));
+                    echo "Insert Cat: ".$data."<br>";
+                    $categories++;
+                }
+            }
             break;
         case 7:
-            $sub_cat = strtolower(preg_replace('/\s+/', '-',stripAccents(trim($data))));
-            if(!in_array($sub_cat,$sub_category)){$sub_category[] = $sub_cat;}
+            if (strlen($data) != 29) {
+                $sub_cat = $data;
+                if (!get_term_by('slug', $data, 'product_cat')) {
+                    $sub_cat = strtolower(preg_replace('/\s+/', '-', stripAccents(trim($data))));
+                    $cat_id = get_term_by('slug', $catMain, 'product_cat');
+                    wp_insert_term($data, 'product_cat', array(
+                        'description' => $data,
+                        'parent' => $cat_id->term_id,
+                        'slug' => $sub_cat
+                    ));
+                    echo "Insert Sub Cat: ".$data."<br>";
+                    $sub_categories++;
+                }
+            }
             break;
         case 9:
-            $sub_sub_cat = strtolower(preg_replace('/\s+/', '-',stripAccents(trim($data))));
-            if(!in_array($sub_sub_cat,$sub_sub_category)){$sub_sub_category[] = $sub_sub_cat;}
+            if (strlen($data) != 29) {
+                $sub_sub_cat = $data;
+                if (!get_term_by('slug', $data, 'product_cat')) {
+                    $sub_sub_cat = strtolower(preg_replace('/\s+/', '-', stripAccents(trim($data))));
+                    $sub_cat_id = get_term_by('slug', $sub_cat, 'product_cat');
+                    wp_insert_term($data, 'product_cat', array(
+                        'description' => $data,
+                        'parent' => $sub_cat_id->term_id,
+                        'slug' => $sub_sub_cat
+                    ));
+                    echo "Inert Sub Sub Cat: ".$data."<br>";
+                    $sub_sub_category++;
+                }
+            }
             break;
         case 11:
-            $sub_sub_sub_cat = strtolower(preg_replace('/\s+/', '-',stripAccents(trim($data))));
-            if(!in_array($sub_sub_sub_cat,$sub_sub_sub_category)){$sub_sub_sub_category[] = $sub_sub_sub_cat;}
+            if (strlen($data) != 29) {
+                $sub_sub_sub_cat = $data;
+                if (!get_term_by('slug', $data, 'product_cat')) {
+                    $sub_sub_sub_cat = strtolower(preg_replace('/\s+/', '-', stripAccents(trim($data))));
+                    $sub_sub_cat_id = get_term_by('slug', $sub_sub_cat, 'product_cat');
+                    wp_insert_term($data, 'product_cat', array(
+                        'description' => $data,
+                        'parent' => $sub_sub_cat_id->term_id,
+                        'slug' => $sub_sub_sub_cat
+                    ));
+                    echo "Insert Sub Sub Sub Cat: ".$data."<br>";
+                    $sub_sub_sub_category++;
+                }
+            }
             break;
         case 14:
             $i = 1;
+            unset($catMain);
+            unset($sub_cat);
+            unset($sub_sub_cat);
+            unset($sub_sub_sub_cat);
             break;
     }
     $i++;
 }
-
-var_dump($category);echo "<br>";
-var_dump($sub_category);echo "<br>";
-var_dump($sub_sub_category);echo "<br>";
-var_dump($sub_sub_sub_category);echo "<br>";
 // Unset the file to call __destruct(), closing the file handle.
 $file = null;
 
+echo "<br>Finished process ---------- <br>";
+echo "Cat added <br>";
+echo "Cat: ".$categories."<br>";
+echo "Sub Cat: ".$sub_categories."<br>";
+echo "Sub Sub Cat: ".$sub_categories."<br>";
+echo "Sub Sub Sub Cat: ".$sub_sub_sub_category."<br>";
 
-function stripAccents($str) {
+
+function stripAccents($str)
+{
     return strtr(utf8_decode($str), utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'), 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
 }
